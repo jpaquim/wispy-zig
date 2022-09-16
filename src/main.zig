066d85b54c91369c8138229496658afda66e8eff
jpaquim <joaopaquim@gmail.com>
2022-09-16 10:22:59 +0100

    Implement jsonStringify for Token type

--- src/main.zig
+++ src/main.zig
@@ -26,19 +26,8 @@ pub fn main() !void {
     const tokens = try lex(allocator, input);
     defer tokens.deinit();
 
-    std.debug.print("{}\n", .{tokens});
+    const tokens_json = try std.json.stringifyAlloc(allocator, tokens.items, .{ .whitespace = .{ .indent = .{ .Space = 2 } }});
+    defer allocator.free(tokens_json);
 
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    // std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    // const stdout_file = std.io.getStdOut().writer();
-    // var bw = std.io.bufferedWriter(stdout_file);
-    // const stdout = bw.writer();
-
-    // try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    // try bw.flush(); // don't forget to flush!
+    std.debug.print("{s}\n", .{tokens_json});
 }
--- src/types/token.zig
+++ src/types/token.zig
@@ -1,7 +1,33 @@
+const std = @import("std");
+
 pub const Token = union(enum) {
     int: i32,
     float: f64,
     identifier: []const u8,
     typed_identifier: []const u8,
     bracket: u8, // '(' | ')' | '[' | ']';
+
+    pub fn jsonStringify(
+        self: Token,
+        opt: std.json.StringifyOptions,
+        w: anytype,
+    ) !void {
+        var jsw = std.json.writeStream(w, 3);
+        if (opt.whitespace) |whitespace| jsw.whitespace = whitespace;
+
+        try jsw.beginObject();
+
+        try jsw.objectField("type");
+        try jsw.emitString(@tagName(self));
+
+        try jsw.objectField("value");
+        switch (self) {
+            .int => |int| try jsw.emitNumber(int),
+            .float => |float| try jsw.emitNumber(float),
+            .identifier, .typed_identifier => |identifier| try jsw.emitString(identifier),
+            .bracket => |bracket| try jsw.emitString(&.{bracket}),
+        }
+
+        try jsw.endObject();
+    }
 };
